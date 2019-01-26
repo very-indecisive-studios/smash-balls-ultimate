91750f52ef2f2af5cd78e509fcbe0ea01392e2a6
shazrin <wldrnz@gmail.com>
2019-01-26 11:11:23 +0800

    Include stable engine headers to be pre compiled

--- src/pch.h
+++ src/pch.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// Standard libraries.
 #include <string>
 #include <vector>
 #include <map>
@@ -9,19 +10,32 @@
 #include <thread>
 #include <memory>
 
+// Windows libraries.
 #include <windows.h>
 #include <WindowsX.h>
 #include <stdlib.h>
 #include <crtdbg.h>
 #include <Mmsystem.h>
 #include <wrl.h>
-
 #include <mfapi.h>
 #include <mfidl.h>
 #include <mfreadwrite.h>
 #include <xaudio2.h>
+#include <XInput.h>
 
+// DirectX libraries.
 #include <d3d9.h>
 #include <d3dx9.h>
 
-#include <XInput.h>
+
+// Stable game engine APIs.
+#include "graphics/graphicsRenderer.h"
+#include "graphics/font.h"
+#include "graphics/texture.h"
+#include "math/math.h"
+#include "input/inputManager.h"
+#include "resource/resourceManager.h"
+// Include ECS once stable.
+// Include Scene once API changes done.
+// Include Audio once API changes done.
+// Include Context once API changes done and everything on top is stable.
